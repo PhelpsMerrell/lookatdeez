a5b7156826e55d56e7f31124cdfb2007094b3b6b
Phelps Merrell <phelps.tomas.merrell@gmail.com>
2025-08-31 21:53:14 -0600

    auth fixes

--- lib/config/auth_config.dart
+++ lib/config/auth_config.dart
@@ -27,7 +27,7 @@ class AuthConfig {
   static const List<String> scopes = [
     'openid',
     'offline_access', // For refresh tokens
-    'https://lookatdeez.onmicrosoft.com/$clientId/access'
+    'https://lookatdeez.onmicrosoft.com/44c46a0b-0c02-4e97-be76-cbe30edc3829/access' // Backend API scope
   ];
   
   // Token storage keys
